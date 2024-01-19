e00a52be02d670de524f06abf217c1bc5ae46d93
kvdmitrieva <katedv02@gmail.com>
2024-01-19 14:49:15 +0300

    configs

--- src/model/__init__.py
+++ src/model/__init__.py
@@ -1,7 +1,9 @@
 from src.model.discriminator import Discriminator
 from src.model.generator import Generator
+from src.model.hifi_generator import HiFiGenerator
 
 __all__ = [
     "Generator",
+    "HiFiGenerator",
     "Discriminator"
 ]
