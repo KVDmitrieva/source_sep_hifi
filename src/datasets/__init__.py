664a6e4abccc923c852c791f8039ee95b14ffdfc
kvdmitrieva <katedv02@gmail.com>
2024-01-24 00:27:10 +0300

    change dataset

--- src/datasets/__init__.py
+++ src/datasets/__init__.py
@@ -1,6 +1,8 @@
 from src.datasets.ljspeech_dataset import LJspeechDataset
+from src.datasets.vctk_dataset import VCTKDataset
 
 
 __all__ = [
-    "LJspeechDataset"
+    "LJspeechDataset",
+    "VCTKDataset"
 ]
