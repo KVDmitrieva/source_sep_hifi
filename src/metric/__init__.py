b19468960c53e70c9e9088b66843b8818cc16ff4
kvdmitrieva <katedv02@gmail.com>
2024-01-24 16:26:09 +0300

    add wmos

--- src/metric/__init__.py
+++ src/metric/__init__.py
@@ -2,11 +2,13 @@ from src.metric.pesq_metric import PESQMetric
 from src.metric.sdr_metric import SDRMetric
 from src.metric.si_sdr_metric import SISDRMetric
 from src.metric.stoi_metric import STOIMetric
+from src.metric.wmos_metric import WMOSMetric
 
 
 __all__ = [
     "PESQMetric",
     "SDRMetric",
     "SISDRMetric",
-    "STOIMetric"
+    "STOIMetric",
+    "WMOSMetric"
 ]
---
+++ src/metric/wmos_metric.py
@@ -0,0 +1,20 @@
+import torch
+
+from wvmos import get_wvmos
+
+from src.metric.base_metric import BaseMetric
+
+
+class WMOSMetric(BaseMetric):
+    def __init__(self, cuda=True, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.model = get_wvmos(cuda=cuda)
+
+    def __call__(self, generator_audio: torch.Tensor, **kwargs):
+        x = generator_audio.detach()
+        x = self.model.processor(x, return_tensors="pt", padding=True, sampling_rate=16000).input_values
+        with torch.no_grad():
+            res = self.model.forward(x)
+        return res.cpu()
+
+
