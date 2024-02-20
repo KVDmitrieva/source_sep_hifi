f67443300168b115e5d5cf28399173b8eb637296
kvdmitrieva <katedv02@gmail.com>
2024-02-20 22:52:07 +0300

    update weights

--- setup.sh
+++ setup.sh
@@ -3,7 +3,7 @@ pip install -r requirements.txt
 
 echo "Download checkpoint"
 gdown "https://drive.google.com/u/0/uc?id=1micEaZlvjWwz-4XCOxn_vbKir03maW9U" -O default_test_model/config.json
-gdown "https://drive.google.com/u/0/uc?id=1HBo9uI71b9VVXw1EZCdrf-CF2_D0GL2D" -O default_test_model/model.pth
+gdown "https://drive.google.com/u/0/uc?id=1hP-DDRU1yv9NR5P9__PRyAE83NogL9xP" -O default_test_model/model.pth
 
 echo "Download vctk test"
 wget "https://datashare.ed.ac.uk/bitstream/handle/10283/2791/noisy_testset_wav.zip"
