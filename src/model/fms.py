7893d914edda7b3c63c991fb0f9b86143b8234fc
kvdmitrieva <katedv02@gmail.com>
2024-03-05 00:41:18 +0300

    change fms

--- src/model/fms.py
+++ src/model/fms.py
@@ -13,9 +13,9 @@ class FMSMaskNet(nn.Module):
         window = torch.hann_window(self.n_fft).to(x.device)
         spectrum = torch.stft(x, n_fft=self.n_fft, window=window, return_complex=False)
         magnitude = torch.sqrt((spectrum ** 2).sum(-1) + 1e-9)
-
-        mul_factor = F.softplus(self.fms(magnitude))
-        spectrum = mul_factor.unsqueeze(-1) * spectrum
+        # mul_factor = F.softplus(self.fms(magnitude))
+        # spectrum = mul_factor.unsqueeze(-1) * spectrum
+        spectrum = self.fms(magnitude)
         out = torch.istft(torch.view_as_complex(spectrum), n_fft=self.n_fft, window=window)
         return out
 
