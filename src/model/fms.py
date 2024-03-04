c7c499db152cfb3c54f60c8b229b11657034cdd8
kvdmitrieva <katedv02@gmail.com>
2024-03-04 23:24:52 +0300

    add fms

---
+++ src/model/fms.py
@@ -0,0 +1,34 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+
+class FMSMaskNet(nn.Module):
+    def __init__(self, n_fft):
+        super().__init__()
+        self.n_fft = n_fft
+        self.fms = FMS(n_fft // 2 + 1)
+
+    def forward(self, x):
+        window = torch.hann_window(self.n_fft).to(x.device)
+        spectrum = torch.stft(x, n_fft=self.n_fft, window=window, return_complex=False)
+        magnitude = torch.sqrt((spectrum ** 2).sum(-1) + 1e-9)
+
+        mul_factor = F.softplus(self.fms(magnitude))
+        spectrum = mul_factor.unsqueeze(-1) * spectrum
+        out = torch.istft(torch.view_as_complex(spectrum), n_fft=self.n_fft, window=window)
+        return out
+
+
+class FMS(nn.Module):
+    def __init__(self, num_channels):
+        super().__init__()
+        self.pooling = nn.AdaptiveAvgPool1d(output_size=1)
+        self.linear = nn.Linear(in_features=num_channels, out_features=num_channels)
+        self.activation = nn.Sigmoid()
+
+    def forward(self, x):
+        s = self.pooling(x).squeeze(2)
+        s = self.linear(s).unsqueeze(2)
+        s = self.activation(s)
+        return s * x + s
--- src/model/generator.py
+++ src/model/generator.py
@@ -4,12 +4,13 @@ from src.model.base_model import BaseModel
 from src.model.hifi_generator import HiFiGenerator
 from src.model.spectral import SpectralUNet, SpectralMaskNet
 from src.model.wave import WaveUNet
+from src.model.fms import FMSMaskNet
 from src.model.utils import fix_shapes_1d
 
 
 class Generator(BaseModel):
     def __init__(self, generator_params, spectral_unet_params=None, wave_unet_params=None,
-                 spectral_mask_params=None, add_spectral=False, concat_audio=True):
+                 spectral_mask_params=None, add_spectral=False, concat_audio=True, use_fms=False):
         super().__init__()
         self.add_spectral = add_spectral
         self.concat_audio = concat_audio
@@ -17,7 +18,9 @@ class Generator(BaseModel):
         self.spec_unet = torch.nn.Identity() if spectral_unet_params is None else SpectralUNet(**spectral_unet_params)
         self.generator = HiFiGenerator(**generator_params)
         self.wave_unet = torch.nn.Identity() if wave_unet_params is None else WaveUNet(**wave_unet_params)
-        self.spec_mask = torch.nn.Identity() if spectral_mask_params is None else SpectralMaskNet(**spectral_mask_params)
+
+        mask_module = FMSMaskNet if use_fms else SpectralMaskNet
+        self.spec_mask = torch.nn.Identity() if spectral_mask_params is None else mask_module(**spectral_mask_params)
 
     def forward(self, mel, audio=None, **batch):
         spec_out = self.spec_unet(mel.unsqueeze(1))
