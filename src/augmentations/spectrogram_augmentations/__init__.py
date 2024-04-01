e65fc582472f8b157e74f1debcff599cffb689b4
kvdmitrieva <katedv02@gmail.com>
2024-04-01 17:43:53 +0300

    remove parsers

--- src/augmentations/spectrogram_augmentations/__init__.py
+++
@@ -1,8 +0,0 @@
-from src.augmentations.spectrogram_augmentations.FrequencyMasking import FrequencyMasking
-from src.augmentations.spectrogram_augmentations.TimeMasking import TimeMasking
-
-
-__all__ = [
-    "FrequencyMasking",
-    "TimeMasking"
-]
--- src/augmentations/wave_augmentations/__init__.py
+++
@@ -1,9 +0,0 @@
-from src.augmentations.wave_augmentations.AddBackgroundNoise import AddBackgroundNoise
-from src.augmentations.wave_augmentations.Shift import Shift
-from src.augmentations.wave_augmentations.Gain import Gain
-
-__all__ = [
-    "AddBackgroundNoise",
-    "Shift",
-    "Gain"
-]
