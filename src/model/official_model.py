60cbeda8f9365b686c3048dd14d3efe554a1e5d1
kvdmitrieva <katedv02@gmail.com>
2024-04-04 18:44:57 +0300

    custom mel

--- src/model/official_model.py
+++ src/model/official_model.py
@@ -234,6 +234,14 @@ class A2AHiFiPlusGeneratorV2(HiFiPlusGenerator):
                 )
             )
 
+    @staticmethod
+    def get_melspec(x):
+        shape = x.shape
+        x = x.view(shape[0] * shape[1], shape[2])
+        x = mel_spectrogram(x, 1024, 80, 16000, 256, 1024, 0, 8000)
+        x = x.view(shape[0], -1, x.shape[-1])
+        return x
+
     def apply_waveunet_a2a(self, x, x_orig):
         if self.waveunet_input == "waveform":
             x_a = self.waveunet_conv_pre(x_orig)
@@ -252,7 +260,7 @@ class A2AHiFiPlusGeneratorV2(HiFiPlusGenerator):
     def forward(self, mel, audio=None, **batch):
         # x_orig = x.clone()
         # x_orig = x_orig[:, :, : x_orig.shape[2] // 1024 * 1024]
-        # x = self.get_melspec(x_orig)
+        mel = self.get_melspec(audio)
         x = self.apply_spectralunet(mel)
         x = self.hifi(x)
         if self.use_waveunet and self.waveunet_before_spectralmasknet:
@@ -905,3 +913,72 @@ class SpectralMaskNet(nn.Module):
 def closest_power_of_two(n):
     return 1 << (n - 1).bit_length()
 
+
+def dynamic_range_compression_torch(x, C=1, clip_val=1e-5):
+    return torch.log(torch.clamp(x, min=clip_val) * C)
+
+
+def spectral_normalize_torch(magnitudes):
+    output = dynamic_range_compression_torch(magnitudes)
+    return output
+
+
+mel_basis = {}
+hann_window = {}
+
+
+def mel_spectrogram(
+    y,
+    n_fft,
+    num_mels,
+    sampling_rate,
+    hop_size,
+    win_size,
+    fmin,
+    fmax,
+    center=False,
+    return_mel_and_spec=False,
+):
+    if isinstance(y, np.ndarray):
+        y = torch.from_numpy(y).unsqueeze(0)
+
+    global mel_basis, hann_window
+    if fmax not in mel_basis:
+        mel = librosa_mel_fn(sr=sampling_rate, n_fft=n_fft, n_mels=num_mels,
+                             fmin=fmin, fmax=fmax)
+        mel_basis[str(fmax) + "_" + str(y.device)] = (
+            torch.from_numpy(mel).float().to(y.device)
+        )
+        hann_window[str(y.device)] = torch.hann_window(win_size).to(y.device)
+
+    y = torch.nn.functional.pad(
+        y.unsqueeze(1),
+        (int((n_fft - hop_size) / 2), int((n_fft - hop_size) / 2)),
+        mode="reflect",
+    )
+    y = y.squeeze(1)
+
+    spec = torch.stft(
+        y,
+        n_fft,
+        hop_length=hop_size,
+        win_length=win_size,
+        window=hann_window[str(y.device)],
+        center=center,
+        pad_mode="reflect",
+        normalized=False,
+        onesided=True,
+        return_complex=False,
+    )
+
+    spec = torch.sqrt(spec.pow(2).sum(-1) + 1e-9)
+
+    mel = torch.matmul(mel_basis[str(fmax) + "_" + str(y.device)], spec)
+    mel = spectral_normalize_torch(mel)
+    result = mel.squeeze()
+
+    if return_mel_and_spec:
+        spec = spectral_normalize_torch(spec)
+        return result, spec
+    else:
+        return result
