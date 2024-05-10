46f4f2d27c74b249eab515d0ea3c6b04a1e599c7
kvdmitrieva <katedv02@gmail.com>
2024-05-10 23:16:39 +0300

    add checkpoins desc

--- setup.sh
+++ setup.sh
@@ -3,25 +3,29 @@ pip install -r requirements.txt
 
 echo "Download checkpoints"
 mkdir checkpoints
-#gdown --folder "https://drive.google.com/drive/folders/1KGnHP5_f7pwelusGoztjpvjyF9LWQ3gL" -O checkpoints/hifi_plusplus  # 80 эпох
-#gdown --folder "https://drive.google.com/drive/folders/1pBUU2Rk-cGOYewrS491hS7nLzsThk5hA" -O checkpoints/hifi_noise     # 80 эпох
-#gdown --folder "https://drive.google.com/drive/folders/1ZuEA0TG8rIiEn-8_nmAa8ddR-KrpnJlf" -O checkpoints/hifi_mrf       # 80 эпох
-#gdown --folder "https://drive.google.com/drive/folders/1NQfny6T2jH0gMUvqFCuL8CuVO0mRii5m" -O checkpoints/hifi_wo_spec   # 80 эпох
-#gdown --folder "https://drive.google.com/drive/folders/16xG39ZUC4qSJZaRMF-GOyHubQH6V8E26" -O checkpoints/hifi_fms       # 90 эпох
-#gdown --folder "https://drive.google.com/drive/folders/1aUurEr9h1T0tk43VyWi6qdamBnQDHi2Z" -O checkpoints/hifi_fms_noise # 45 эпох
-#
-#gdown --folder "https://drive.google.com/drive/folders/1KGnHP5_f7pwelusGoztjpvjyF9LWQ3gL" -O checkpoints/hifi_stream           # 50 эпох
-#gdown --folder "https://drive.google.com/drive/folders/1pBUU2Rk-cGOYewrS491hS7nLzsThk5hA" -O checkpoints/hifi_noise_stream     # 30 эпох
-#gdown --folder "https://drive.google.com/drive/folders/1ZuEA0TG8rIiEn-8_nmAa8ddR-KrpnJlf" -O checkpoints/hifi_mrf_stream       # 60 эпох
-#gdown --folder "https://drive.google.com/drive/folders/1NQfny6T2jH0gMUvqFCuL8CuVO0mRii5m" -O checkpoints/hifi_wo_spec_stream   # 40 эпох
-#gdown --folder "https://drive.google.com/drive/folders/16xG39ZUC4qSJZaRMF-GOyHubQH6V8E26" -O checkpoints/hifi_fms_stream       # 70 эпох
 
 gdown --fuzzy "https://drive.google.com/file/d/1BPEnaTEEyxpK2w-yNlSt6fJgl2l0qNiJ/view?usp=drive_link" -O checkpoints.zip
 unzip -q checkpoints.zip
 
-#mkdir checkpoints/hifi_official
-#wget "https://github.com/SamsungLabs/hifi_plusplus/releases/download/w/se.pth" -O checkpoints/hifi_official/generator.pth
-#cp "src/configs/hifi_official.json" "checkpoints/hifi_official/config.json"  discriminator
+# checkpoints
+#  - hifi_official
+#  - hifi_plusplus  (80 эпох)
+#  - hifi_fixed     (100 эпох)
+#  - hifi_wo_spec   (80 эпох)
+#  - hifi_noise     (80 эпох)
+#  - hifi_mrf       (80 эпох)
+#  - hifi_fms       (90 эпох)
+#  - hifi_fms_noise (45 эпох)
+#  - hifi_mrf_fms   (120 эпох)
+
+#  - hifi_stream           (50 эпох)
+#  - hifi_fixed_stream     (20 эпох)
+#  - hifi_noise_stream     (30 эпох)
+#  - hifi_mrf_stream       (80 эпох)
+#  - hifi_wo_spec_stream   (80 эпох)
+#  - hifi_fms_stream       (70 эпох)
+
+# - hifi_context           (20 эпох)
 
 
 echo "Download vctk test"
