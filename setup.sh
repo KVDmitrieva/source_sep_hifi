549cc600683510795f88b5a45388fdb53321771b
kvdmitrieva <katedv02@gmail.com>
2024-06-17 17:19:18 +0300

    update link

--- setup.sh
+++ setup.sh
@@ -4,7 +4,7 @@ pip install -q -r requirements.txt
 echo "Download checkpoints"
 mkdir checkpoints
 
-gdown --fuzzy "https://drive.google.com/file/d/1nLLhb-MykFwXdFDcoaOrmWhz3z9Urt6E/view?usp=drive_link" -O checkpoints.zip
+gdown --fuzzy "https://drive.google.com/file/d/1IeqsvF0rv79KCjWovbbhGmoUHGMcSFNT/view?usp=sharing" -O checkpoints.zip
 unzip -q checkpoints.zip
 
 # checkpoints
