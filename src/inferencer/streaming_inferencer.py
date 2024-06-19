927e89658e5d6830374b0ba704c329ea012e346b
kvdmitrieva <katedv02@gmail.com>
2024-06-19 18:20:31 +0300

    update streaming inferencer

--- src/inferencer/streaming_inferencer.py
+++ src/inferencer/streaming_inferencer.py
@@ -12,19 +12,24 @@ from src.datasets.streamer import FastFileStreamer
 
 
 class StreamingInferencer(Inferencer):
-    def __init__(self, config, chunk_size, window_delta, segment_size=None):
+    def __init__(self, config, chunk_size : int, window_delta : int, mode: str = "overlap_add", normalize_chunk=False, segment_size: int = None):
+        assert mode in ["overlap_add", "overlap_add_sin", "overlap_nonintersec"], "invalid overlap mode"
         super().__init__(config, segment_size)
 
-        # TODO: get chunk_size & window_delta from config
         self.chunk_size = chunk_size
         self.window_delta = window_delta
+        self.normalize_chunk = normalize_chunk
+
+        if mode == "overlap_add":
+            self.overlap = self.overlap_add
+        elif mode == "overlap_add_sin":
+            self.overlap = self.overlap_add_sin
+        else:
+            self.overlap = self.overlap_nonintersec
 
         self.streamer = FastFileStreamer(chunk_size, window_delta)
 
-    def denoise_streaming_audio(self, noisy_path: str, out_path: str = "result.wav",
-                                mode: str = "overlap_add", normalize_chunk: bool = True):
-        assert mode in ["overlap_add", "overlap_add_sin", "overlap_nonintersec"], "invalid overlap mode"
-
+    def denoise_audio(self, noisy_path: str, out_path: str = "result.wav"):
         noisy_audio = self._load_audio(noisy_path)
         noisy_audio = self._cut_audio(noisy_audio, new_ind=True)
 
@@ -44,85 +49,13 @@ class StreamingInferencer(Inferencer):
 
             outputs.append(gen_chunk.cpu().squeeze())
 
-        if mode == "overlap_add":
-            gen_audio = self.overlap_add(outputs, self.window_delta, self.chunk_size)
-        elif mode == "overlap_add_sin":
-            gen_audio = self.overlap_add_sin(outputs, self.window_delta, self.chunk_size)
-        else:
-            gen_audio = self.overlap_nonintersec(outputs, self.window_delta, self.chunk_size)
-
+        gen_audio = self.overlap(outputs, self.window_delta, self.chunk_size)
         gen_audio = gen_audio.unsqueeze(0)
+        
         if out_path is not None:
             torchaudio.save(out_path, gen_audio, self.target_sr)
 
         return gen_audio
-
-    def denoise_streaming_dir(self, noisy_dir: str, out_dir: str = "output", mode: str = "overlap_add"):
-        assert Path(noisy_dir).exists(), "invalid noisy_path"
-
-        if not Path(out_dir).exists():
-            Path(out_dir).mkdir(exist_ok=True, parents=True)
-
-        files = sorted(os.listdir(noisy_dir))
-        noisy_dir, out_dir = Path(noisy_dir), Path(out_dir)
-
-        for file_name in tqdm(files, desc="Process file"):
-            noisy_path = str(noisy_dir / file_name)
-            out_path = str(out_dir / file_name)
-            _ = self.denoise_streaming_audio(noisy_path, out_path, mode)
-
-    def validate_streaming_audio(self, noisy_path: str, clean_path: str, out_path: str = "result.wav", mode: str = "overlap_add", verbose=True):
-        gen_audio = self.denoise_streaming_audio(noisy_path, out_path, mode)
-        clean_audio = self._load_audio(clean_path)
-        clean_audio = self._cut_audio(clean_audio, new_ind=False)
-
-        result = {"file": noisy_path.split('/')[-1]}
-        if self.wmos is not None:
-            result["wv-mos"] = self.wmos(gen_audio.to(self.device))
-
-        if noisy_path != clean_path:
-            to_pad = clean_audio.shape[1] - gen_audio.shape[1]
-            gen_audio = torch.nn.functional.pad(gen_audio, (0, to_pad))
-            metrics = self.composite_eval(gen_audio, clean_audio)
-            result.update(metrics)
-
-        if verbose:
-            for key, val in result.items():
-                print(f"{key}: {val}")
-
-        return result
-
-    def validate_streaming_dir(self, noisy_dir: str, clean_dir: str, out_dir: str = "output", mode: str = "overlap_add", verbose=True):
-        assert Path(noisy_dir).exists(), "invalid noisy dir"
-        assert Path(clean_dir).exists(), "invalid clean dir"
-
-        if not Path(out_dir).exists():
-            Path(out_dir).mkdir(exist_ok=True, parents=True)
-
-        files = sorted(os.listdir(noisy_dir))
-        noisy_dir, clean_dir, out_dir = Path(noisy_dir), Path(clean_dir), Path(out_dir)
-
-        results = []
-        metrics_score = {}
-
-        for file_name in tqdm(files, desc="Process file"):
-            noisy_path = str(noisy_dir / file_name)
-            clean_path = str(clean_dir / file_name)
-            out_path = str(out_dir / file_name)
-            result = self.validate_streaming_audio(noisy_path, clean_path, out_path, mode, verbose=False)
-
-            for key, val in result.items():
-                if key != "file":
-                    metrics_score[key] = metrics_score.get(key, 0.0) + val
-
-            results.append(result)
-
-        if verbose:
-            for key, val in metrics_score.items():
-                print(f"{key}: {val / len(files):.3f}")
-
-        with (out_dir / "result.txt").open("w") as f:
-            json.dump(results, f, indent=2)
 
     @staticmethod
     def overlap_add(chunks, window_delta, chunk_size):
@@ -140,8 +73,7 @@ class StreamingInferencer(Inferencer):
             if i == 0:
                 res[:chunk_size] = ch
             else:
-                overlap = ch[:chunk_size - window_delta] * window + res[window_delta * i:window_delta * (
-                            i - 1) + chunk_size] * (1 - window)
+                overlap = ch[:chunk_size - window_delta] * window + res[window_delta * i:window_delta * (i - 1) + chunk_size] * (1 - window)
                 res[window_delta * i:window_delta * (i - 1) + chunk_size] = overlap
                 res[window_delta * (i - 1) + chunk_size:window_delta * i + chunk_size] = ch[chunk_size - window_delta:]
 
--- streaming_test.py
+++ streaming_test.py
@@ -14,34 +14,17 @@ DEFAULT_CHECKPOINT_PATH = ROOT_PATH / "default_test_model" / "checkpoint.pth"
 
 if __name__ == "__main__":
     args = argparse.ArgumentParser(description="PyTorch Template")
-    args.add_argument(
-        "-c", "--config", default=None, type=str, help="config file noisy_path (default: None)",
-    )
-    args.add_argument(
-        "-r", "--resume", default=str(DEFAULT_CHECKPOINT_PATH.absolute().resolve()),
-        type=str, help="noisy_path to latest checkpoint (default: None)",
-    )
-    args.add_argument(
-        "-d", "--device", default=None, type=str, help="indices of GPUs to enable (default: all)",
-    )
-    args.add_argument(
-        "-o", "--output", default="output", type=str, help="Dir to write results",
-    )
-    args.add_argument(
-        "-n", "--noisy_dir", default=None, type=str, help="Path to noisy audios",
-    )
-    args.add_argument(
-        "-t", "--target_dir", default=None, type=str, help="Path to target audios",
-    )
-    args.add_argument(
-        "-m", "--overlap_mode", default=None, type=str, help="Overlap mode",
-    )
-    args.add_argument(
-        "-s", "--chunk_size", default=4096, type=int, help="Chunk size",
-    )
-    args.add_argument(
-        "--segment_size", default=None, type=int, help="Max audio len"
-    )
+
+    args.add_argument("-c", "--config", default=None, type=str, help="config file noisy_path (default: None)",)
+    args.add_argument("-r", "--resume", default=str(DEFAULT_CHECKPOINT_PATH.absolute().resolve()), type=str, help="path to latest checkpoint (default: None)")
+    args.add_argument("-d", "--device", default=None, type=str, help="indices of GPUs to enable (default: all)",)
+    args.add_argument("-o", "--output", default="output", type=str, help="Dir to write results")
+    args.add_argument("-n", "--noisy_dir", default=None, type=str, help="Path to noisy audios")
+    args.add_argument("-t", "--target_dir", default=None, type=str, help="Path to target audios")
+    args.add_argument("-j", "--json_path", default=None, type=str, help="Path to json with audio paths")
+    args.add_argument("-m", "--overlap_mode", default=None, type=str, help="Overlap mode",)
+    args.add_argument("-s", "--chunk_size", default=4096, type=int, help="Chunk size",)
+    args.add_argument("--segment_size", default=None, type=int, help="Max audio len")
 
     args = args.parse_args()
 
@@ -56,12 +39,14 @@ if __name__ == "__main__":
         with Path(args.config).open() as f:
             config.config.update(json.load(f))
 
-    # TODO: add params to config
     inferencer = StreamingInferencer(config, chunk_size=args.chunk_size,
                                      window_delta=args.chunk_size // 2,
-                                     segment_size=args.segment_size)
-
-    if args.target_dir is None:
-        inferencer.denoise_streaming_dir(args.noisy_dir, args.output, args.overlap_mode)
+                                     segment_size=args.segment_size,
+                                     mode=args.overlap_mode)
+    
+    if args.json_path is not None:
+        inferencer.validate_json(args.json_path, args.output)
+    elif args.target_dir is None:
+        inferencer.denoise_dir(args.noisy_dir, args.output)
     else:
-        inferencer.validate_streaming_dir(args.noisy_dir, args.target_dir, args.output, args.overlap_mode)
+        inferencer.validate_dir(args.noisy_dir, args.target_dir, args.output)
