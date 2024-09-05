2a4a8b789e4c6ba1f3ed20382b4577eb53046908
kvdmitrieva <katedv02@gmail.com>
2024-09-05 22:00:25 +0300

    fix stoi metric

--- src/metric/stoi_metric.py
+++ src/metric/stoi_metric.py
@@ -5,9 +5,9 @@ from src.metric.base_metric import BaseMetric
 
 
 class STOIMetric(BaseMetric):
-    def __init__(self, fs=16000, *args, **kwargs):
+    def __init__(self, fs=16000, extended=True, *args, **kwargs):
         super().__init__(*args, **kwargs)
-        self.stoi = ShortTimeObjectiveIntelligibility(fs)
+        self.stoi = ShortTimeObjectiveIntelligibility(fs, extended=extended)
 
     def __call__(self, generator_audio: Tensor, target_audio: Tensor, **kwargs):
         generator_audio = generator_audio.cpu().detach()
